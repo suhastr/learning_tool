164dafd98c0bb706dd83101b11d84162ccf58783
abcd <36911828+suhastr@users.noreply.github.com>
2025-02-12 01:00:41 -0500

    changes ongoing

--- ll_project/settings.py
+++ ll_project/settings.py
@@ -140,12 +140,12 @@ config = Config()
 if config.is_valid_platform():
     ALLOWED_HOSTS.append('.platformsh.site')
     if config.appDir:
-    STATIC_ROOT = Path(config.appDir) / 'static'
+        STATIC_ROOT = Path(config.appDir) / 'static'
     if config.projectEntropy:
-    SECRET_KEY = config.projectEntropy
+        SECRET_KEY = config.projectEntropy
     if not config.in_build():
-    db_settings = config.credentials('database')
-    DATABASES = {
+        db_settings = config.credentials('database')
+        DATABASES = {
     'default': {
     'ENGINE': 'django.db.backends.postgresql',
     'NAME': db_settings['path'],
